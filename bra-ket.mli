218acc5402e21ae5074acfe6b8108542b7ef8670
Joel Williamson <joel.s.williamson@gmail.com>
2014-09-28 12:24:51 -0400

    Added normalization and expectation value

--- bra-ket.mli
+++ bra-ket.mli
@@ -1,6 +1,8 @@
 module type Integrable = sig
 	(** This is the underlying space. **)
 	type t
+	(** Multiply a value from the underlying space with a value from the target **)
+	val mul : t -> Complex.t -> Complex.t
 	(** Functions on the underlying space **)
 	type f = t -> Complex.t
 	(** Take the integral over the entire space **)
@@ -13,11 +15,13 @@ module OneDimensional = struct
 	let integrate f =
 		List.fold_left Complex.add Complex.zero
 			( List.map f [-5;-4;-3;-2;-1;0;1;2;3;4;5])
+	let mul x y = Complex.mul {Complex.re = (float_of_int x); im = 0.} y
 end;;
 
 let (<|) f g = fun x -> f (g x)
 
 module Make_braket (M : Integrable) = struct
+	open Complex
 	type ket = M.f
 
 	type bra = M.f -> Complex.t
@@ -25,11 +29,27 @@ module Make_braket (M : Integrable) = struct
 	type operator = ket -> ket
 
 	let make_bra (k:ket):bra =
-		(fun k' -> M.integrate (fun x -> Complex.mul (Complex.conj (k x)) (k' x)))
+		(fun k' -> M.integrate (fun x -> mul (conj (k x)) (k' x)))
 	
 	let bra_op (b:bra) (o:operator) : bra = b <| o
 	let (!<) (b:bra) : bra = b
 	let ($|) (b:bra) (o:operator) = bra_op b o
 	let (|$) (b:bra) (k:ket) = b k
 	let ($>) (v:Complex.t) _ = v
+
+	let normalize (k:ket) =
+		let normalization_factor =
+			let current_norm = (make_bra k) k in
+			div one (Complex.sqrt current_norm)
+		in (fun x -> Complex.mul normalization_factor (k x))
+	
+	let expectation_value (op:operator) k =
+		(make_bra k) |$ op k
+	
+	let position_operator:operator =
+		fun k -> fun x -> M.mul x (k x)
 end;;
+
+module Simple = Make_braket(OneDimensional)
+let psi x = {Complex.re = (float_of_int (5 - x)) ; im = 0.} 
+let psi' = Simple.make_bra(psi)
